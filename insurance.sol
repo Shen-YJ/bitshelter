c41293ffe7a44032498675a534c08d9bab2f5c3a
Davide <dcarboni@gmail.com>
2016-05-27 01:40:03 +0100

    restructured with status

--- insurance.sol
+++ insurance.sol
@@ -3,43 +3,47 @@
 contract Insurance {
 	uint premium;
 	address oracle;
-	uint    liability;
+	uint    compensation;
 	address insurer;
 	address subscriber;
+	enum    state { CREATED, SUBSCRIBED, ACTIVE, CLAIMED, TERMINATED}
+	state status;
+	uint constant profit = 200 finney;
+	uint duration;
+	uint expireTime;
 	event Subscription(address,uint);
 	
-	function Insurance(address o,uint p,uint l) {
-		oracle = o;
-		premium = p;
-		liability = l;
-
+	function Insurance(address anOracle,uint aPremium,uint comp, uint ttl) {
+		oracle = anOracle;
+		premium = aPremium;
+		compensation = comp;
+		duration = ttl;
+        status = state.CREATED;
 	}
 	function subscribe() {
+		
+		if(status != state.CREATED) throw;
+		
 		Subscription(msg.sender,msg.value);
 		if(msg.value==premium){
 			subscriber=msg.sender;
-			
+			status = state.SUBSCRIBED;
 		}
 		else throw;
 	}
 	
 	function back(){
-		if(msg.value==liability){
+		if(status != state.SUBSCRIBED) throw;
+		
+		if(msg.value==compensation){
 			insurer=msg.sender;
-			insurer.send(premium - 200 finney);
+			insurer.send(premium - profit);
+			status = state.ACTIVE;
+			expireTime = now + duration;
 		}
 		else throw;
 	}
 	
-	function oracleSays(bool verdict){
-		if (msg.sender != oracle) throw;
-		
-		if(verdict){
-			subscriber.send(liability);
-		}
-		else{
-			insurer.send(liability);
-		}
-	}
+
 
 }
